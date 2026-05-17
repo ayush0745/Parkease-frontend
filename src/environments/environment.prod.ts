f00789c0c95b98ab664df0eb6a6fa6c36d8ae5fe
ayush0745 <ayushrathour800@gmail.com>
2026-05-18 05:14:17 +0530

    [Ayush Rathour] Refactor .updated environment.ts file

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://parkease-api.onrender.com/api/v1', // Replace with your actual Render API Gateway URL
+  apiUrl: 'https://parkease-1-9z9j.onrender.com/api/v1',
   googleClientId: '356169181305-fu3c7b7mk6utie1fmoc5hkr3qarqii59.apps.googleusercontent.com',
   stripePublicKey: 'pk_test_YOUR_STRIPE_PUBLIC_KEY',
   razorpayKey: 'rzp_test_SmXLLXzt0TRU4N',
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8080/api/v1',
+  apiUrl: 'https://parkease-1-9z9j.onrender.com/api/v1',
   googleClientId: '356169181305-fu3c7b7mk6utie1fmoc5hkr3qarqii59.apps.googleusercontent.com',
   // Add these for better functionality
   stripePublicKey: 'pk_test_YOUR_STRIPE_PUBLIC_KEY',
